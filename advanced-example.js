44970c9a626a5d8fd81da549c30a30e7afef9b9e
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-01-23 15:35:15 -0600

    Update advanced-example.js

--- advanced-example.js
+++ advanced-example.js
@@ -1,25 +1,17 @@
 
 
-//importCoreJs();
-
-function myFunctionExample() {
-  let myURL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Male_and_female_chicken_sitting_together.jpg/220px-Male_and_female_chicken_sitting_together.jpg';
-  console.log(Utilities.base64Encode(Utilities.newBlob([...sheetFetch(myURL)].map(x => x.codePointAt())).getBytes()));
-  //console.log([...myURL].map(x=>x.codePointAt()));
-}
-
-function sheetFetch(url) {
-  const sheetFetchFile = SpreadsheetApp.openById('118Ty5Y5humiZz3G9xOHbW9zgZCh3NkdSRI3oLIZ3lQQ');
-  const sheetFetchService = sheetFetchFile.getSheetByName("fetch");
+function importData(url) {
+  const importDataFile = SpreadsheetApp.openById('118Ty5Y5humiZz3G9xOHbW9zgZCh3NkdSRI3oLIZ3lQQ');
+  const importDataService = importDataFile.getSheetByName("importDataSheet");
   let col;
   let cell;
   const columns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   try {
-    col = getColumnLock(sheetFetchFile, sheetFetchService);
-    cell = sheetFetchFile.setCurrentCell(sheetFetchService.getRange(`${col}2`));
+    col = getColumnLock(importDataFile, importDataService);
+    cell = importDataFile.setCurrentCell(importDataService.getRange(`${col}2`));
     cell.setValue(`=IMPORTDATA("${url}","${String.fromCharCode(57840)}")`);
     SpreadsheetApp.flush();
-    const cells = sheetFetchService.getRange(2, columns.indexOf(col) + 1, sheetFetchService.getLastRow(), 1).getValues();
+    const cells = importDataService.getRange(2, columns.indexOf(col) + 1, importDataService.getLastRow(), 1).getValues();
     const fetchedValue = cells.join('\n').trim();
     return fetchedValue;
   } catch (e) {
@@ -28,13 +20,13 @@ function sheetFetch(url) {
     (async () => {
       await "immediate";
       cell.setValue('');
-      cell = sheetFetchFile.setCurrentCell(sheetFetchService.getRange(`${col}1`));
+      cell = importDataFile.setCurrentCell(importDataService.getRange(`${col}1`));
       cell.setValue('');
     })();
   }
 }
 
-function getColumnLock(sheetFetchFile, sheetFetchService) {
+function getColumnLock(importDataFile, importDataService) {
   const myID = new Date().getTime();
   const columns = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let col = 'A';
@@ -42,7 +34,7 @@ function getColumnLock(sheetFetchFile, sheetFetchService) {
 
   for (let i = 0; i < 1000; i++) {
     col = columns[getRandomInt(26)];
-    cell = sheetFetchFile.setCurrentCell(sheetFetchService.getRange(`${col}1`));
+    cell = importDataFile.setCurrentCell(importDataService.getRange(`${col}1`));
     cell.setValue(myID);
     if (cell.getValues()[0] == myID) {
       return col;
